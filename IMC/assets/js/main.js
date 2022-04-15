9451caf6c94e25166e903758209b520b2920643d
Victor Elias <svvictorelias@gmail.com>
2022-04-15 17:35:38 -0300

    adicionado validação de peso e altura

--- IMC/assets/js/main.js
+++ IMC/assets/js/main.js
@@ -1,8 +1,39 @@
 const form = document.querySelector('#form')
 
+
 form.addEventListener('submit', resetavalor)
 
-//foi adicionado preventDefault para que a pagina não recarregue
+
 function resetavalor(evento){
+    //foi adicionado preventDefault para que a pagina não recarregue
     evento.preventDefault()
+
+    const peso = document.querySelector('#peso').value
+    const altura = document.querySelector('#altura').value
+    validaPeso(peso)
+    imc = calculaImc(peso, altura)
+    console.log(imc)
+
+    
+}
+
+function validaPeso(peso){
+    if(Number(peso)){
+        alert('é número')
+    }else{
+        alert('n é número')
+    }
+}
+
+function validaAltura(altura){
+    if(Number(altura)){
+        alert('é número')
+    }else{
+        alert('n é número')
+    }
+}
+
+function calculaImc(peso, altura){
+    imc = (peso/(altura**2)).toFixed(2)
+    return imc
 }
