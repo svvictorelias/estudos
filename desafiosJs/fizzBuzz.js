93e949b26d5a52e9b0d74c8779de8c1fc9346e10
Victor Elias <svvictorelias@gmail.com>
2022-04-17 10:51:14 -0300

    treinando logica com fizzbuzz

--- desafiosJs/fizzBuzz.js
+++ desafiosJs/fizzBuzz.js
@@ -5,3 +5,24 @@
 //Numero é divisivel por 3 e 5 = FizzBuzz
 //Numero não é divisivel por 3 e 5 = Retorna o prprio número
 //Use uma função com números de 0 a 100
+
+
+
+function fizzBuzz(n){
+    
+    if(typeof n !== 'number'){
+        return n
+    }else if(n%5 === 0 && n%3 === 0){
+        return 'FizzBuzz'
+    }else if(n%5 === 0){
+        return 'Buzz'
+    }else if(n%3 === 0){
+        return 'Fizz'
+    }else{
+        return n
+    }
+}
+
+for(let i = 0; i<=100; i++){
+    console.log(i, fizzBuzz(i))
+}
