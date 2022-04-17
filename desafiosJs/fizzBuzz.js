dd8fae37f8d686c27a71029bc8a944bb51cd80d9
Victor Elias <svvictorelias@gmail.com>
2022-04-16 21:53:17 -0300

    Adicionado exercicio tipo de imagem e descrição de FizzBuzz

---
+++ desafiosJs/fizzBuzz.js
@@ -0,0 +1,7 @@
+//Escreva uma função que recebe um número e
+//Retorne o seguinte:
+//Numero é divisivel por 3 = Fizz
+//Numero é divisivel por 5 = Buzz
+//Numero é divisivel por 3 e 5 = FizzBuzz
+//Numero não é divisivel por 3 e 5 = Retorna o prprio número
+//Use uma função com números de 0 a 100
