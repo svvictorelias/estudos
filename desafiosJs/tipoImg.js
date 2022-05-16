27471746fcbc37acd663a74def900e4502de18fd
Victor Elias <svvictorelias@gmail.com>
2022-05-16 16:18:18 -0300

    retirado arquivos da branch main

--- desafiosJs/tipoImg.js
+++
@@ -1,8 +0,0 @@
-//Escreva uma função chamada ePaisagem que recebe dois argumentos, 
-//largura e altura de uma imagem(number). 
-//Retorne true se a imagem estiver em modo paisagem(largura>altura)
-
-const ePaisagem = (largura, altura) => largura>altura
-console.log(ePaisagem(1280, 720))
-console.log(ePaisagem(1080, 1920))
-
